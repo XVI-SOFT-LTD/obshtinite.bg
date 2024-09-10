66c318b57dc5fe61dcd2f0952fe0b7e1f3017cf5
Teodor Nachev <tenachev24@gmail.com>
2024-09-10 23:08:38 +0300

    datatable

--- app/Http/Controllers/Admin/AdminLandmarksController.php
+++ app/Http/Controllers/Admin/AdminLandmarksController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Admin\AdminController;
 use App\Helpers\Helper;
 use App\Models\Landmark;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\DynamicAdminDataTable;
 
 class AdminLandmarksController extends AdminController
 {
@@ -26,6 +27,39 @@ class AdminLandmarksController extends AdminController
         $this->basicBreadcrumbs = [
             ['text' => 'Списък забележителност', 'url' => route($this->routes . '.index')],
         ];
+    }
+
+     public function index(Request $request)
+    {
+        $landmark = $this->model->getAdminAll($request);
+
+        $dataTable = new DynamicAdminDataTable();
+        $dataTable->setRoute($this->routes);
+        $dataTable->setColumns([
+            'fullname' => 'Име на забележителността',
+            'logo' => 'Снимка',
+            'active' => 'Активна',
+            'created_at' => 'Създадена на',
+            'updated_at' => 'Променена на',
+        ]);
+        $dataTable->setSkipSortableIds([2]);
+        $dataTable->setRows($landmark, [
+            'name' => function ($landmark) {
+                return $landmark->i18n->name;
+            },
+            'logo' => function ($landmark) {
+                return '<img src="' . $landmark->getLogo() . '" class="img-thumbnail" style="max-height: 40px;">';
+            },
+            'active' => function ($landmark) {
+                return $landmark->active ? 'Да' : 'Не';
+            },
+            'created_at',
+            'updated_at',
+        ]);
+
+        $dataTable->setBreadcrumbs($this->basicBreadcrumbs);
+
+        return $dataTable->render($this->pluralPageTitle);
     }
 
 
